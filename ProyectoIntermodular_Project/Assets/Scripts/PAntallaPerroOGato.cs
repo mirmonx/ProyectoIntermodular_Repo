03a8f9bed2386d12cd28cbcd14048e2a0deec5a0
Cris <cristina.tornero.de.baro@gmail.com>
2026-04-02 13:03:57 +0200

    Cambios realizados por Cris
    (conecto algunas rutas de backend con frontend)

--- ProyectoIntermodular_Project/Assets/Scripts/PAntallaPerroOGato.cs
+++ ProyectoIntermodular_Project/Assets/Scripts/PAntallaPerroOGato.cs
@@ -1,22 +1,47 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
 
-public class PAntallaPerroOGato : MonoBehaviour
+public class PantallaPerroOGato : MonoBehaviour
 {
     public TMP_Text textoSaludo;
 
+    string url = "http://localhost:8080/seleccionAnimal";
+
     void Start()
     {
-        textoSaludo.text = "Hola " + PantallaNombre.nombreMascota; // muestra el nombre de la anterior pantalla
+        textoSaludo.text = "Hola " + PantallaNombre.nombreMascota;
     }
 
     public void SeleccionarGato()
     {
         Debug.Log(PantallaNombre.nombreMascota + " es un gato");
+        StartCoroutine(EnviarAnimal("gato"));
     }
 
     public void SeleccionarPerro()
     {
         Debug.Log(PantallaNombre.nombreMascota + " es un perro");
+        StartCoroutine(EnviarAnimal("perro"));
+    }
+
+    IEnumerator EnviarAnimal(string animal)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("animal", animal);
+
+        UnityWebRequest request = UnityWebRequest.Post(url, form);
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Respuesta servidor: " + request.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log("Error: " + request.error);
+        }
     }
 }
--- ProyectoIntermodular_Project/Assets/Scripts/PantallaNombre.cs
+++ ProyectoIntermodular_Project/Assets/Scripts/PantallaNombre.cs
@@ -1,30 +1,79 @@
-//Script pantalla Nombre
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 using System.Collections;
 
 public class PantallaNombre : MonoBehaviour
 {
+    // es donde el usuario escribe el nombre
     public TMP_InputField inputNombre;
+
+    // texto que muestro por  pantalla
     public TextMeshProUGUI textoTitulo;
 
+    // variable estática para guardar el nombre y usarlo en otras escenas
     public static string nombreMascota;
+
+    // URL de ruta que he creado en js
+    string url = "http://localhost:8080/guardarMascota";
+
 
     public void GuardarNombre()
     {
+        // guardo el nombre 
         nombreMascota = inputNombre.text;
 
+        // mensaje por pantalla
         textoTitulo.text = "Hola!!! Encantado/a de conocerte " + nombreMascota;
 
+        // se ve el nombre en la consola de unity
         Debug.Log("Nombre introducido: " + nombreMascota);
 
-        StartCoroutine(CambiarEscena()); //funcion para ejecutar y pausar el juego
+        // envio el nombre al backend
+        StartCoroutine(EnviarNombre());
+
+        // cambio de escena
+        StartCoroutine(CambiarEscena());
     }
 
-    IEnumerator CambiarEscena() //he puesto IENUMERATOR porque es una funcion que se ejecuta por partes 
+
+    // corrutina para enviar datos al servidor
+    IEnumerator EnviarNombre()
     {
-        yield return new WaitForSeconds(3f); //yield significa pausar el scrit pero sin congelar (sigue funcionando el juego)
-        SceneManager.LoadScene("GatoOperro");// La escena en naranja, es la escena de Unity que abre
+        // formulario para enviar datos (POST)
+        WWWForm form = new WWWForm();
+
+        // campo "nombre" con el valor introducido por usuario
+        form.AddField("nombre", nombreMascota);
+
+        // Creo la petición POST al backend
+        UnityWebRequest request = UnityWebRequest.Post(url, form);
+
+        // envio petición y espero respuesta (asincronia)
+        yield return request.SendWebRequest();
+
+        // bucle para si ha ido bien muestre la respues en la consola del servidor
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            
+            Debug.Log("Respuesta servidor: " + request.downloadHandler.text);
+        }
+        else
+        {
+            // por si hay error
+            Debug.Log("Error: " + request.error);
+        }
+    }
+
+
+    //corrutina para cambiar de escena
+    IEnumerator CambiarEscena()
+    {
+        //para esperar 3 segundos
+        yield return new WaitForSeconds(3f);
+
+        // cargo la escena siguiente
+        SceneManager.LoadScene("GatoOperro");
     }
 }
